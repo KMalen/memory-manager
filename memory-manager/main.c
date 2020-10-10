b3ef90f62696efcd5da9ca95008560b37ee513dc
Kiryl Malenchik <kirillmalen@gmail.com>
2020-10-10 21:38:01 +0300

    Initial Commit

---
+++ memory-manager/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  memory-manager
+//
+//  Created by Kiril Malenchik on 10/10/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
