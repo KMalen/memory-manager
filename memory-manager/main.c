708019407bf77e3c67138dd6c519dce6b6408301
Kiryl Malenchik <kirillmalen@gmail.com>
2020-10-13 16:07:59 +0300

    add memory-logic memory.c, main.c

--- memory-manager/main.c
+++ memory-manager/main.c
@@ -11,51 +11,69 @@
 
 #include <stdio.h>
 
+
+
+
+void errorAbort(int error_code);
+
 int main(int argc, const char * argv[]) {
     m_init(1,500);
     int error_code;
+    
+    m_id chunk_1 = m_malloc(13, &error_code);
+    errorAbort(error_code);
+    
+    m_id chunk_2 = m_malloc(20, &error_code);
+    errorAbort(error_code);
 
-      m_id chunk_1 = m_malloc(13, &error_code);
-      if (error_code != M_ERR_OK) abort();
+    m_id chunk_3 = m_malloc(50, &error_code);
+    errorAbort(error_code);
+    
+    m_write(chunk_1, "Hello World!", 13, &error_code);
+    errorAbort(error_code);
 
-      m_id chunk_2 = m_malloc(20, &error_code);
-      if (error_code != M_ERR_OK) abort();
+    m_write(chunk_2, "Operating Systems", 18, &error_code);
+    errorAbort(error_code);
 
-      m_id chunk_3 = m_malloc(50, &error_code);
-      if (error_code != M_ERR_OK) abort();
+    m_write(chunk_3, "Super dumb memory allocator", 28, &error_code);
+    errorAbort(error_code);
 
-      m_write(chunk_1, "Hello World!", 13, &error_code);
-      if (error_code != M_ERR_OK) abort();
+    char buffer[50];
 
-      m_write(chunk_2, "Operating Systems", 18, &error_code);
-      if (error_code != M_ERR_OK) abort();
+    m_read(chunk_1, buffer, 13, &error_code);
+    errorAbort(error_code);
+    printf("%s\n", buffer);
 
-      m_write(chunk_3, "Super dumb memory allocator", 28, &error_code);
-      if (error_code != M_ERR_OK) abort();
+    m_read(chunk_2, buffer, 18, &error_code);
+    errorAbort(error_code);
+    printf("%s\n", buffer);
 
-      char buffer[50];
+    m_read(chunk_3, buffer, 28, &error_code);
+    errorAbort(error_code);
+    printf("%s\n", buffer);
 
-      m_read(chunk_1, buffer, 13, &error_code);
-      if (error_code != M_ERR_OK) abort();
-      printf("%s\n", buffer);
+    m_free(chunk_1, &error_code);
+    errorAbort(error_code);
 
-      m_read(chunk_2, buffer, 18, &error_code);
-      if (error_code != M_ERR_OK) abort();
-      printf("%s\n", buffer);
+    m_free(chunk_2, &error_code);
+    errorAbort(error_code);
 
-      m_read(chunk_3, buffer, 28, &error_code);
-      if (error_code != M_ERR_OK) abort();
-      printf("%s\n", buffer);
-
-      m_free(chunk_1, &error_code);
-      if (error_code != M_ERR_OK) abort();
-
-      m_free(chunk_2, &error_code);
-      if (error_code != M_ERR_OK) abort();
-
-      m_free(chunk_3, &error_code);
-      if (error_code != M_ERR_OK) abort();
+    m_free(chunk_3, &error_code);
+    errorAbort(error_code);
 }
+
+void errorAbort(int error_code){
+    
+    if (error_code != M_ERR_OK){
+        printf("%d\n", error_code);
+        abort();
+    }
+}
+
+//void toArray(m_id* idChunk, int sizeChunk, int index){
+//    arrayOfChunks[index].m_id = idChunk;
+//    arrayOfChunks[index].size = sizeChunk;
+//}
 
 // TODO:
 
--- memory-manager/memory/memory.c
+++ memory-manager/memory/memory.c
@@ -14,14 +14,26 @@ char* _g_allocator_memory = NULL;
 int _g_allocator_memory_size = 0;
 int _g_bytes_allocated = 0;
 
+struct Chunks {
+    void* m_id;
+    int size;
+};
+
+struct Chunks arrayOfChunks[10];
+int indexChunk = 0;
 
 m_id m_malloc(int size_of_chunk, m_err_code* error) {
+    
   if (_g_bytes_allocated + size_of_chunk > _g_allocator_memory_size) {
     *error = M_ERR_ALLOCATION_OUT_OF_MEMORY;
     return NULL;
   }
 
   _g_bytes_allocated += size_of_chunk;
+    
+    arrayOfChunks[indexChunk].m_id = _g_allocator_memory + _g_bytes_allocated;
+    arrayOfChunks[indexChunk].size = size_of_chunk;
+    indexChunk++;
 
   *error = M_ERR_OK;
   return _g_allocator_memory + _g_bytes_allocated;
@@ -34,12 +46,32 @@ void m_free(m_id ptr, m_err_code* error) {
 
 
 void m_read(m_id read_from_id,void* read_to_buffer, int size_to_read, m_err_code* error) {
+    
+    for (int i = 0; i < indexChunk; i++) {
+        if (read_from_id == arrayOfChunks[i].m_id) {
+            if (size_to_read > arrayOfChunks[i].size) {
+                *error = M_ERR_OUT_OF_BOUNDS;
+                return;
+            }
+        }
+    }
+    
   memcpy(read_to_buffer, read_from_id, size_to_read);
   *error = M_ERR_OK;
 }
 
 
 void m_write(m_id write_to_id, void* write_from_buffer, int size_to_write, m_err_code* error) {
+    
+    for (int i = 0; i < indexChunk; i++) {
+        if (write_to_id == arrayOfChunks[i].m_id) {
+            if (size_to_write > arrayOfChunks[i].size) {
+                *error = M_ERR_ALLOCATION_OUT_OF_MEMORY;
+                return;
+            }
+        }
+    }
+    
   memcpy(write_to_id, write_from_buffer, size_to_write);
   *error = M_ERR_OK;
 }
